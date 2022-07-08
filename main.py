c91942ef36a8a7104f9cf9360f917430759805d8
VladTomici14 <vladtomici@gmail.com>
2022-07-08 09:20:02 +0300

    Eliminated some arguments from the parser. Created some functions for the conversion to array for the pictures and for uploading pictures from the local computer. Started to organize the way the code will look.

--- main.py
+++ main.py
@@ -1,44 +1,85 @@
+from predictor import MoveNetPredictor
 from messages import Messages
 import tensorflow as tf
+import tensorflow_hub
 import argparse
 import cv2
 
 
-def load_model(model):
+# TODO: add comments and the pydocs
+
+def load_model():
     """
     This function will load the model.
-        :param model: it can be the "float16" / "int8" models
         :return: the final model, which can be used to make predictions
     """
 
     try:
         # ------- setting up the model path ------
-        input_size = 256
-        if model == "float16":
-            model_path = "models/lite-model_movenet_singlepose_thunder_tflite_float16_4.tflite"
+        thunder_model_url = "https://tfhub.dev/google/movenet/singlepose/thunder/4"
+        module = tensorflow_hub.load(thunder_model_url)
+        model = module.signatures['serving_default']
 
-        elif model == "int8":
-            model_path = "models/lite-model_movenet_singlepose_thunder_tflite_int8_4.tflite"
+        Messages().success("The model was loaded")
 
-        # ------- initializing the TFLite interpreter ---------
-        interpreter = tf.lite.Interpreter(model_path=model_path)
-        interpreter.allocate_tensors()
+        return model
 
     except Exception:
-        Messages().error("Unable to load the model. Please choose a valid one (float32 / int8)")
+        Messages().error(Exception)
+        return None
 
 
-def detect_on_image(image_path):
-    image = cv2.imread(image_path)
+def detect_on_image(tensor_image, model):
+    """
+    This function can do detection for one picture
+        :param tensor_image: The image in a TF array supported format.
+        :param model: The model which we want to use
+        :return: The coordinates of the key-points if a person was found in the image.
+                 None if no one was detected in the picture.
+    """
 
-    pass
+    if model is not None:
+        input_image = tf.cast(tensor_image, dtype=tf.int32)
+
+        outputs = model(input_image)
+
+        keypoints_with_scores = outputs['output_0'].numpy()
+
+        return keypoints_with_scores
+
+    else:
+        Messages().error("The model was not uploaded correctly!")
+        return None
+
+def image2array(tensor_image):
+    # Resize and pad the image to keep the aspect ratio and fit the expected size.
+    input_image = tf.expand_dims(tensor_image, axis=0)
+    input_image = tf.image.resize_with_pad(input_image, 256, 256)
+
+    return input_image
+
+
+def load_image(image_path):
+    image = tf.io.read_file(image_path)
+    image = tf.image.decode_jpeg(image)
+
+    return image
 
 
 if __name__ == "__main__":
-    # ----- argparsing the input image path ------
+    # ----- argparse the input image path ------
     ap = argparse.ArgumentParser()
     ap.add_argument("--image", required=True, help="path of the input image")
-    ap.add_argument("--model", default="float16", help="which model to be uploaded")
+    ap.add_argument("--draw", default=True, help="Do you want to draw the keypoints on the image? (True/False)")
     args = vars(ap.parse_args())
 
-    load_model(args["model"])
+    model = load_model()
+    loaded_image = load_image(args["image"])
+    image_array = image2array(loaded_image)
+    key_points = detect_on_image(image_array, model)
+
+    if args["draw"]:
+        predictor = MoveNetPredictor()
+        predictor.draw_keypoints_on_image(loaded_image, key_points)
+
+    print(key_points)
