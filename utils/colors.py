d1eaf77fb6d224645a786dc57e4586cfef24c2a5
VladTomici14 <vladtomici@gmail.com>
2022-07-20 01:43:08 +0300

    Added new colors.

--- utils/colors.py
+++ utils/colors.py
@@ -2,4 +2,9 @@
 class BGRColors:
     BLUE = (255, 0, 0)
     RED = (0, 0, 255)
+    GREEN = (0, 255, 0)
     YELLOW = (96, 241, 254)
+    CYAN = (227, 254, 125)
+    PURPLE = (252, 20, 140)
+    ORANGE = (53, 121, 242)
+    PINK = (242, 5, 250)
